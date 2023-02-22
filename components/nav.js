92ca800acb13a5d04677e105fb3cde99ea61681b
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-02-21 18:17:16 -0600

    Authorization

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,18 @@
-export default function Nav(){
-    return(
-        <div>
-            <h1>Nav</h1>
-        </div>
+import Link from "next/link";
+
+export default function Nav() {
+    return (
+        <nav className = "flex justify-between items-center py-10">
+            <Link href = "/">
+                <button className="text-lg font-medium">Creative Writes</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                <Link legacyBehavior href = {"/auth/login"}>
+                    <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+                        Join Now
+                    </a>
+                </Link>
+            </ul>
+        </nav>
     );
-};
+}
